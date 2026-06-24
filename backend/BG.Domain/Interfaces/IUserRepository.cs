c0c65afe6b463371ae529e37d88b206cbebbf9f2
Am0rr <itsaizel468@gmail.com>
2026-06-24 15:14:29 +0300

    feat(domain): add IUserRepository interface

---
+++ backend/BG.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+using BG.Domain.Entities.Identity;
+
+namespace BG.Domain.Interfaces;
+
+public interface IUserRepository : IBaseRepository<User>
+{
+    Task<User> GetByEmailAsync(string email, CancellationToken cancellationToken = default);
+}
