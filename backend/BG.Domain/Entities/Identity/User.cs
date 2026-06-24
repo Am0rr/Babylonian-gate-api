5bd4d7bdd4217ab17378b19a11f85439a67b3920
Am0rr <itsaizel468@gmail.com>
2026-06-24 14:40:40 +0300

    refactor(domain): replase Username field in User entitiy with First and Last names

--- backend/BG.Domain/Entities/Identity/User.cs
+++ backend/BG.Domain/Entities/Identity/User.cs
@@ -4,22 +4,25 @@ namespace BG.Domain.Entities.Identity;
 
 public class User : BaseEntity
 {
-    public string Username { get; private set; } = null!;
+    public string FirstName { get; private set; } = null!;
+    public string LastName { get; private set; } = null!;
     public string Email { get; private set; } = null!;
     public string PasswordHash { get; private set; } = null!;
     public UserRole Role { get; private set; }
 
     protected User() { }
 
-    public User(string username, string email, string passwordHash, UserRole role = UserRole.Client)
+    public User(string firstName, string lastName, string email, string passwordHash, UserRole role = UserRole.Client)
     {
-        Username = username;
+        FirstName = firstName;
+        LastName = lastName;
         Email = email;
         PasswordHash = passwordHash;
         Role = role;
     }
 
-    public void ChangeUsername(string newUsername) => Username = newUsername;
+    public void ChangeFirstName(string newFirstName) => FirstName = newFirstName;
+    public void ChangeLastName(string newLastName) => LastName = newLastName;
     public void ChangeEmail(string newEmail) => Email = newEmail;
     public void ChangeRole(UserRole newRole) => Role = newRole;
 }
